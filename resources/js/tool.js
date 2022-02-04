e319eba4a5d030dc04828fd52a2e1f21c2fdf6e9
Ahmed Kandil <sa@tomorrowsystem.com>
2022-02-04 12:46:50 +0100

    Update devDependencies

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,3 +1,3 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('nova-s3-multipart-upload', require('./components/Tool'))
+    Vue.component('nova-s3-multipart-upload', require('./components/Tool').default)
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,10 @@
-let mix = require('laravel-mix')
+let mix = require('laravel-mix');
 
 mix.setPublicPath('dist')
     .js('resources/js/tool.js', 'js')
-    .sass('resources/sass/tool.scss', 'css', {
-        includePaths: ["node_modules"]
-    })
+    .vue()
+    .sass('resources/sass/tool.scss', 'css', {}, [
+        require('autoprefixer'),
+        require('postcss-logical'),
+        require('postcss-dir-pseudo-class'),
+    ]);
