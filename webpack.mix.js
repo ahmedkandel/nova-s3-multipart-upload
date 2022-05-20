75dd2902e8c4ddd51e9e953b28bdc07b61e0fbc0
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-05-20 22:36:23 +0200

    Nova 4 JS configuration update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,14 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
 
-mix.setPublicPath('dist')
-    .js('resources/js/tool.js', 'js')
-    .vue()
+require('./nova.mix')
+
+mix
+  .setPublicPath('dist')
+  .js('resources/js/tool.js', 'js')
+  .vue({ version: 3 })
     .sass('resources/sass/tool.scss', 'css', {}, [
         require('autoprefixer'),
         require('postcss-logical'),
         require('postcss-dir-pseudo-class'),
-    ]);
+    ])
+  .nova('ahmedkandel/nova-s3-multipart-upload');
